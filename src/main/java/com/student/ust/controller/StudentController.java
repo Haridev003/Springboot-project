b126fb9107854df2fd3af8c86b9375c8a0b25c6c
Haridev003 <235784@ust-global.com>
2022-11-03 12:24:45 +0530

    pushing

--- src/main/java/com/student/ust/controller/StudentController.java
+++ src/main/java/com/student/ust/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.student.ust.controller;
 
+import com.student.ust.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
 
 }
