77eb830e5b4b601cdd68e19b45bdae626845b30c
Haridev003 <235784@ust-global.com>
2022-11-03 12:43:52 +0530

    Commit

--- src/main/java/com/student/ust/controller/StudentController.java
+++ src/main/java/com/student/ust/controller/StudentController.java
@@ -1,13 +1,30 @@
 package com.student.ust.controller;
 
+import com.student.ust.entity.Student;
 import com.student.ust.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RestController
 public class StudentController {
 
     @Autowired
     StudentService studentService;
+
+    public ResponseEntity<Student> get(@PathVariable Integer id){
+    try {
+        Student student = studentService.getStudentById(id);
+        return new ResponseEntity<Student>(student, HttpStatus.OK);
+    }
+    catch (NoSuchElementException e){
+        return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+
+    }
+    }
 
 }
--- src/main/java/com/student/ust/service/StudentService.java
+++ src/main/java/com/student/ust/service/StudentService.java
@@ -1,8 +1,13 @@
 package com.student.ust.service;
 
+import com.student.ust.entity.Student;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+    public Student getStudentById(Integer id) {
+
+        return null;
+    }
 }
