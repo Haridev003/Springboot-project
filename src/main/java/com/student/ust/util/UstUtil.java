e346850aa64162d5297a4d4734c30663cc7704d6
Haridev003 <235784@ust-global.com>
2022-11-15 12:42:30 +0530

    Commit

--- src/main/java/com/student/ust/util/UstUtil.java
+++ src/main/java/com/student/ust/util/UstUtil.java
@@ -9,8 +9,17 @@ import java.security.NoSuchAlgorithmException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * The type Ust util.
+ */
 public class UstUtil {
 
+    /**
+     * Email vaild int.
+     *
+     * @param email the email
+     * @return the int
+     */
     public static int emailVaild(String email) {
         String regex = "^([A-Za-z0-9+_.-]+)@([A-Za-z0-9]+)\\.([a-z]+)$";
         Pattern pattern = Pattern.compile(regex);
@@ -21,6 +30,13 @@ public class UstUtil {
             throw new InvalidException();
         }
     }
+
+    /**
+     * Password valid int.
+     *
+     * @param password the password
+     * @return the int
+     */
     public static int passwordValid(String password) {
         //String regex = "^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!&$%&? \"]).*$";
         String regex = "^(?=(?:.*\\d){3,})(?=\\S+$)(?=.*[@#$%^&+=])(?=(?:.*[A-Za-z]){3,})(?=.*[A-Z]).{7,}$";
@@ -36,6 +52,13 @@ public class UstUtil {
 
     }
 
+    /**
+     * Get sha byte [ ].
+     *
+     * @param input the input
+     * @return the byte [ ]
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     */
     public static byte[] getSHA(String input) throws NoSuchAlgorithmException
     {
         // Static getInstance method is called with hashing SHA
@@ -47,6 +70,12 @@ public class UstUtil {
         return md.digest(input.getBytes(StandardCharsets.UTF_8));
     }
 
+    /**
+     * To hex string string.
+     *
+     * @param hash the hash
+     * @return the string
+     */
     public static String toHexString(byte[] hash)
     {
         // Convert byte array into signum representation
